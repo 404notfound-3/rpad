c233eb2206993b05cbf3709c080e78fd7e44b6ff
Rahul Meena <52858429+404notfound-creator@users.noreply.github.com>
2020-06-12 16:17:48 +0530

    version 0.03
    Changed font_color icon
    Edited align_center icon
    Edited align_left icon
    Edited align_right icon
    Edited bold icon
    Edited italic icon
    Edited underline icon

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-import cx_Freeze
-import sys
-import os 
+import os, sys, cx_Freeze
+
 base = None
 
 if sys.platform == 'win32':
@@ -9,13 +8,13 @@ if sys.platform == 'win32':
 os.environ['TCL_LIBRARY'] = r"C:\Users\sword\AppData\Local\Programs\Python\Python38\tcl\tcl8.6"
 os.environ['TK_LIBRARY'] = r"C:\Users\sword\AppData\Local\Programs\Python\Python38\tcl\tk8.6"
 
-executables = [cx_Freeze.Executable("rpad.py", base=base, icon="icon.ico")]
+executables = [cx_Freeze.Executable("rpad.py", base=base, icon=r"icons\icon.ico")]
 
 
 cx_Freeze.setup(
     name = "Rpad Text Editor",
-    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["icon.ico",'tcl86t.dll','tk86t.dll', 'icons']}},
-    version = "0.02",
+    options = {"build_exe": {"packages":["tkinter","os"], "include_files":['tcl86t.dll','tk86t.dll', 'icons']}},
+    version = "0.03",
     description = "Tkinter Application",
     executables = executables
     )
